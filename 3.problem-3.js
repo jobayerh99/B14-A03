469ea7d724166234e88dde7569fe535a7d4c2ba5
jobayerh99 <jobayerh99@gmail.com>
2026-08-10 08:48:41 +0600

    hit the third case

--- 3.problem-3.js
+++ 3.problem-3.js
@@ -1,5 +1,27 @@
 const countHashtags = (caption) => {
-    if(typeof caption !== 'string'){
+
+    if (typeof caption !== 'string') {
         return "Invalid"
     }
+
+    const separetedWord = caption.split(" ")
+
+    const hashTagWord = separetedWord.filter(word => word.startsWith('#'))
+
+    const howManyTags = hashTagWord.length
+
+    const cleanWord = hashTagWord.map(word => word.slice(1))
+
+    let theBigTag = cleanWord[0] || "";
+
+    cleanWord.forEach((word) => {
+        if (word.length > theBigTag.length) {
+            theBigTag = word
+        }
+    });
+
+    return {
+        hashtagCount: howManyTags,
+        longestTag: theBigTag
+    };
 }
