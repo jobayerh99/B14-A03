509040d221048360158e062f436cc1261e664964
jobayerh99 <jobayerh99@gmail.com>
2026-08-10 07:30:43 +0600

    problem 2 solved

--- 2.problem-2.js
+++ 2.problem-2.js
@@ -1,3 +1,12 @@
 const filterActiveUsers = (users) => {
+    
+    if (!Array.isArray(users) || users.length === 0) {
+        return "Invalid"
+    } else if (!users.every(user => 'isActive' in user)) {
+        return "Invalid"
+    }
 
+    const findTheActiveUser = users.filter(user => user.isActive)
+
+    return findTheActiveUser;
 }
