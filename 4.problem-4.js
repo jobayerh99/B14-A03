a439b4859181e962c5210c8318cbac6eaabe32e3
jobayerh99 <jobayerh99@gmail.com>
2026-08-10 14:24:16 +0600

    done all the problem solved

--- 4.problem-4.js
+++ 4.problem-4.js
@@ -1,3 +1,18 @@
-const bonusScore = (scores) =>{
+const bonusScore = (scores) => {
 
+    if (!Array.isArray(scores) || scores.length === 0) {
+        return "Invalid"
+    }
+
+    const isAllOk = scores.every(score => typeof score === 'number');
+    
+    if (!isAllOk) {
+        return "Invalid"
+    }
+
+    const addBonus = scores.map(score => score + 10);
+
+    const total = addBonus.reduce((score, currentScore) => score + currentScore, 0);
+
+    return total;
 }
